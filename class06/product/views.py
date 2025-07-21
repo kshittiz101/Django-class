e471ecddec3c4c789b5b85e2cf6b39bed3d32e8e
kshittiz101 <chaudharykshittiz950@gmail.com>
2025-07-21 21:40:45 +0545

    getting all dynamic value

--- class06/product/views.py
+++ class06/product/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'product/index.html')
+    products = Product.objects.all()
+    # Queryset => collection or list of query instance or model instance
+    # model intance => actual value or object
+    # <QuerySet [<Product: Laptop>, <Product: Mobile>]>
+    print(products)
+    context = {'key': products, 'course': 'Django'}
+
+    # return render(request, 'product/index.html', {'products': products})
+    return render(request, 'product/index.html', context=context)
