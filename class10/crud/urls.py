151597a6be5d7a0f2258c253e68d9e660652a99d
kshittiz101 <chaudharykshittiz950@gmail.com>
2025-07-29 21:47:56 +0545

    create is done

--- class10/crud/urls.py
+++ class10/crud/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import student_table
+from .views import student_table, register_student
 urlpatterns = [
     path('stud-table/', student_table, name="student-table"),
+    path('register/', register_student, name="register"),
 ]
--- class10/crud/views.py
+++ class10/crud/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Student
 
 # Create your views here.
@@ -20,5 +20,23 @@ def student_table(request):
     # Student ko all data fetch garnu xa vane talko code use garne
     # orm = modelName.objects.all()
     students = Student.objects.all()
-    context = {'students': students}
+    context = {'keys': students}
     return render(request, 'crud/student-table.html', context)
+
+
+def register_student(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        age = request.POST.get('age')
+        email = request.POST.get('email')
+        gender = request.POST.get('gender')
+        # orm --> modelName.objects.create(modelAttributes=Value )
+
+        success = Student.objects.create(
+            stu_name=name, stu_age=age,
+            stu_email=email, gender=gender
+        )
+        if success:
+            return redirect('student-table')
+
+    return render(request, 'crud/student-register.html')
